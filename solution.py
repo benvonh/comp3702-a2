78e51cf14a9d786f96a7632731332d5f878b8b99
benvonh <benjaminvonsnarski@gmail.com>
2022-09-18 10:02:47 +1000

    Fixed state set initialisation

--- solution.py
+++ solution.py
@@ -1,5 +1,6 @@
 import sys
 import time
+import numpy as np
 from constants import *
 from environment import *
 from state import State
@@ -20,74 +21,87 @@ class Solver:
 
     def __init__(self, environment: Environment):
         self.environment = environment
-        #
-        # TODO: Define any class instance variables you require (e.g. dictionary mapping state to VI value) here.
-        #
-        pass
+        # Inputs
+        self.S = None
+        self.A = ROBOT_ACTIONS
+        self.P = None
+        self.R = None
+        # Outputs
+        self.V = None
+        self.PI = None
+        # Parameters
+        self.k = 0 # time step iteration
+        self.epsilon = environment.epsilon # convergence threshold
+        self.discount = environment.gamma # discount factor
 
-    # === Value Iteration ==============================================================================================
+    # === values Iteration =============================================================================================
 
     def vi_initialise(self):
         """
-        Initialise any variables required before the start of Value Iteration.
+        Initialise any variables required before the start of values Iteration.
         """
-        #
-        # TODO: Implement any initialisation for Value Iteration (e.g. building a list of states) here. You should not
-        #  perform value iteration in this method.
-        #
-        # In order to ensure compatibility with tester, you should avoid adding additional arguments to this function.
-        #
-        pass
+        self.k = 0
+
+        self.S = dict()
+        for player_rot in ROBOT_ORIENTATIONS:
+            for player_row in range(self.environment.n_rows):
+                for player_col in range(self.environment.n_cols):
+                    for widget_row in range(self.environment.n_rows):
+                        for widget_col in range(self.environment.n_cols):
+                            for widget_rot in WIDGET_ORIENTS[self.environment.widget_types[0]]:
+                                try:
+                                    self.S[
+                                        State(
+                                            self.environment,
+                                            (player_row, player_col), player_rot,
+                                            ((widget_row, widget_col),), (widget_rot,)
+                                        )
+                                     ] = 0
+                                except Exception as e:
+                                    #print(e)
+                                    continue
+
+        #self.values = [self.states]
+        print(self.S)
 
     def vi_is_converged(self):
         """
-        Check if Value Iteration has reached convergence.
+        Check if values Iteration has reached convergence.
         :return: True if converged, False otherwise
         """
-        #
-        # TODO: Implement code to check if Value Iteration has reached convergence here.
-        #
-        # In order to ensure compatibility with tester, you should avoid adding additional arguments to this function.
-        #
-        pass
+        #vk   = np.array(self.S[self.k].values())
+        #vk_1 = np.array(self.S[self.k-1].values())
+        #return np.all((np.abs(vk - vk_1) < self.epsilon) == True)
+        return True
 
     def vi_iteration(self):
         """
-        Perform a single iteration of Value Iteration (i.e. loop over the state space once).
+        Perform a single iteration of values Iteration (i.e. loop over the state space once).
         """
-        #
-        # TODO: Implement code to perform a single iteration of Value Iteration here.
-        #
-        # In order to ensure compatibility with tester, you should avoid adding additional arguments to this function.
-        #
-        pass
+        self.k += 1
 
     def vi_plan_offline(self):
         """
-        Plan using Value Iteration.
+        Plan using values Iteration.
         """
         # !!! In order to ensure compatibility with tester, you should not modify this method !!!
         self.vi_initialise()
+        self.vi_iteration() # FIXME: SUS
         while not self.vi_is_converged():
             self.vi_iteration()
 
-    def vi_get_state_value(self, state: State):
+    def vi_get_state_values(self, state: State):
         """
         Retrieve V(s) for the given state.
         :param state: the current state
         :return: V(s)
         """
-        #
-        # TODO: Implement code to return the value V(s) for the given state (based on your stored VI values) here. If a
-        #  value for V(s) has not yet been computed, this function should return 0.
-        #
-        # In order to ensure compatibility with tester, you should avoid adding additional arguments to this function.
-        #
+        #return self.values[self.k][state]
         pass
 
     def vi_select_action(self, state: State):
         """
-        Retrieve the optimal action for the given state (based on values computed by Value Iteration).
+        Retrieve the optimal action for the given state (based on values computed by values Iteration).
         :param state: the current state
         :return: optimal action for the given state (element of ROBOT_ACTIONS)
         """
@@ -147,7 +161,7 @@ class Solver:
 
     def pi_select_action(self, state: State):
         """
-        Retrieve the optimal action for the given state (based on values computed by Value Iteration).
+        Retrieve the optimal action for the given state (based on values computed by values Iteration).
         :param state: the current state
         :return: optimal action for the given state (element of ROBOT_ACTIONS)
         """
@@ -159,9 +173,8 @@ class Solver:
         pass
 
     # === Helper Methods ===============================================================================================
-    #
-    #
-    # TODO: Add any additional methods here
-    #
-    #
-
+    def get_one_widget_permutation(self):
+        for widget_row in range(self.environment.n_rows):
+            for widget_col in range(self.environment.n_cols):
+                for widget_rot in WIDGET_ORIENTS[self.environment.widget_types]:
+                    yield (widget_row, widget_col), widget_rot
